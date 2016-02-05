36e3b0a64b55a61d577ae938f68f8299c5b8cf64
Fabio Carvalho <fccoelho7@gmail.com>
2016-02-05 10:58:14 -0200

    Header meta configs

--- header.php
+++ header.php
@@ -7,9 +7,11 @@ endif;
 <!DOCTYPE HTML>
 <html lang="en-US">
 	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width,maximum-scale=1.0">
-		<title><?php wp_title(); ?></title>
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<?php if ( ! get_option( 'site_icon' ) ) : ?>
+			<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" rel="shortcut icon" />
+		<?php endif; ?>
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" type="image/x-icon" />
 		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
